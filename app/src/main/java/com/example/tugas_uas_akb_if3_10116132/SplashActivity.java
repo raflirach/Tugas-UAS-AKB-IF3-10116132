8747ba36b51721213080e30e833949fbae5590d9
raflirach <rafli060395@email.unikom.ac.id>
2019-08-10 11:50:42 +0700

    Add Splash screen, Login And Register

--- app/src/main/java/com/example/tugas_uas_akb_if3_10116132/SplashActivity.java
+++ app/src/main/java/com/example/tugas_uas_akb_if3_10116132/SplashActivity.java
@@ -1,3 +1,9 @@
+/*
+    Tanggal pengerjaan : 09/08/2019
+    Nim : 10116132
+    Nama : Rafli Rachmawandi
+    Kelas : IF-3 (AKB-3)
+ */
 package com.example.tugas_uas_akb_if3_10116132;
 
 import android.content.Intent;
--- app/src/main/java/com/example/tugas_uas_akb_if3_10116132/View/LoginView.java
+++ app/src/main/java/com/example/tugas_uas_akb_if3_10116132/View/LoginView.java
@@ -1,3 +1,10 @@
+/*
+    Tanggal pengerjaan : 10/08/2019
+    Nim : 10116132
+    Nama : Rafli Rachmawandi
+    Kelas : IF-3 (AKB-3)
+ */
+
 package com.example.tugas_uas_akb_if3_10116132.View;
 
 public interface LoginView {
